c93fafe3800794228a0cd17c28073d96c194d9ee
Alex <alexellis2@gmail.com>
2016-07-17 22:10:52 +0100

    motors

--- part3/drive_v1/app.py
+++ part3/drive_v1/app.py
@@ -1,14 +1,19 @@
 import time
+from motors import Motors
 from wiimotereader import WiimoteReader
 
 class Robot:
     def __init__(self, reader):
         self.reader = reader
         self.live = True
+        self.move_delay = 0.7
+        self.turn_delay = 0.25
+        self.motors = Motors()
+
     def control_loop(self):
         while(self.live == True):
             cmd = reader.read()
-            self.live = cmd.Execute()
+            self.live = cmd.Execute(self.motors, self.move_delay, self.turn_delay)
             time.sleep(0.025)
 
 reader = WiimoteReader()
---
+++ part3/drive_v1/motors.py
@@ -0,0 +1,19 @@
+import time
+import explorerhat
+
+class Motors:
+  def forwards(self):
+      explorerhat.motor.one.forwards()
+      explorerhat.motor.two.forwards()
+  def stop(self):
+      explorerhat.motor.one.stop()
+      explorerhat.motor.two.stop()
+  def backwards(self):
+      explorerhat.motor.one.backwards()
+      explorerhat.motor.two.backwards()
+  def left(self):
+      explorerhat.motor.one.backwards()
+      explorerhat.motor.two.forwards()
+  def right(self):
+      explorerhat.motor.one.forwards()
+      explorerhat.motor.two.backwards()
