da328ebe93fa93064bc001e74473033ed4ab81b6
Alex <alexellis2@gmail.com>
2016-07-17 10:47:28 +0100

    repl

--- part3/motor_tests/motor_repl_v1.py
+++ part3/motor_tests/motor_repl_v1.py
@@ -7,13 +7,22 @@ move_delay = 1
 turn_delay = 0.5
 
 def evaluate(line):
-    if(line == "f")
+    if(line == "f"):
         motors.forwards()
         time.sleep(move_delay)
-    if(line == "l")
+        motors.stop()
+    if(line == "l"):
         motors.left()
         time.sleep(turn_delay)
-    # Also fill in code for turning right and backwards
+        motors.stop()
+    if(line == "b"):
+        motors.backwards()
+        time.sleep(move_delay)
+        motors.stop()
+    if(line == "r"):
+        motors.right()
+        time.sleep(turn_delay)
+        motors.stop()
 
 while(last != 'q'):
     sys.stdout.write("Command: ")
