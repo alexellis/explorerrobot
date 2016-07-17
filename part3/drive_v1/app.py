c49e141c1b7c0f8e8e6cdd510c9ba4025cdf01a7
Alex <alexellis2@gmail.com>
2016-07-17 22:08:50 +0100

    Wiimote

--- part3/drive_v1/app.py
+++ part3/drive_v1/app.py
@@ -3,7 +3,7 @@ from wiimotereader import WiimoteReader
 
 class Robot:
     def __init__(self, reader):
-        self.reader = WiimoteReader()
+        self.reader = reader
         self.live = True
     def control_loop(self):
         while(self.live == True):
@@ -11,7 +11,7 @@ class Robot:
             self.live = cmd.Execute()
             time.sleep(0.025)
 
-reader = wiimotereader()
+reader = WiimoteReader()
 robot = Robot(reader)
 
 robot.control_loop()
